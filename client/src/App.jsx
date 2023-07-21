6279559dd8a9bb5ead5f465dd7d2ca8c36ff9ea7
Kailash Choudhary <kailashjat604@gmail.com>
2023-07-22 02:57:59 +0530

    feat: add user profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom"
 import Layout from "./components/Layout"
 import Home, {loader as homePageLoader} from "./pages/Home"
+import UserProfile, {loader as userProfileLoader} from "./pages/UserProfile"
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Layout />}>
     <Route element={<Home />} index loader={homePageLoader} />
+    <Route element={<UserProfile />} path="profile" loader={userProfileLoader} />
   </Route>
 ))
 
---
+++ client/src/pages/UserProfile.jsx
@@ -0,0 +1,8 @@
+
+export async function loader() {
+  return null;
+}
+
+export default function UserProfile() {
+  return <div>User Profile Goes Here...</div>
+}
