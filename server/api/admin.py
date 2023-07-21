af7117c51ea03052abfd5a27f369e912a64ac67f
Kailash Choudhary <kailashjat604@gmail.com>
2023-07-22 02:20:59 +0530

    feat: add tables for skills, themes and ideas

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Skill, Theme, Idea
 
 # Register your models here.
+admin.site.register(Skill)
+admin.site.register(Theme)
+admin.site.register(Idea)
