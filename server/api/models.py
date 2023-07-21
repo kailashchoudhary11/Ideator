3acf50ab3b64aeccdea08f0c06a0bdb454192cfb
Kailash Choudhary <kailashjat604@gmail.com>
2023-07-22 04:28:22 +0530

    fix: update model representation

--- server/api/models.py
+++ server/api/models.py
@@ -6,10 +6,22 @@ class Skill(models.Model):
   name = models.CharField(max_length=100)
   users = models.ManyToManyField(User, null=True, blank=True)
 
+  def __str__(self):
+      return self.name
+
 class Theme(models.Model):
   name = models.CharField(max_length=100)
+
+  def __str__(self):
+      return self.name
+  
 
 class Idea(models.Model):
   idea = models.TextField()
   skills = models.ManyToManyField(Skill)
   themes = models.ManyToManyField(Theme)
+
+  def __str__(self):
+    if len(self.idea) > 30:
+      return self.idea[:30] + "..."
+    return self.idea
