4bf5f1288ee2d2d08194ebadcd1202403d424b37
Vidip Ghosh <vidip2001@gmail.com>
2023-07-21 23:11:24 +0530

    Added Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,3 +1,27 @@
+import './Navbar.css'
+
 export default function Navbar() {
-  return <div>Navbar Goes Here...</div>
+  const handleHamburgerClick = () => {
+    const navLink = document.querySelector('.nav__link');
+    navLink.classList.toggle('hide');
+  };
+  return (
+    <div>
+      <nav className="nav">
+        <a href="/" className="logo">Ideator</a>
+        <div className="hamburger" onClick={handleHamburgerClick}>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+
+        <div className="nav__link hide">
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">SignIn</a>
+          <a href="">Contact</a>
+        </div>
+      </nav>
+    </div>
+  )
 }
