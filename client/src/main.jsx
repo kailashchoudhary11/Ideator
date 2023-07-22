737c1eb8e7dafac0e9acc048b70cd2c35a096f07
Vidip Ghosh <vidip2001@gmail.com>
2023-07-22 13:21:38 +0530

    Removed routes from main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,34 +1,7 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import React from 'react';
+import App from './App.jsx'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import App from './App';
-import Layout from "./components/Layout"
-import Home, {loader as homePageLoader} from "./pages/Home"
-import Login, {action as loginPageAction} from "./pages/Login"
-import Register, {action as registerPageAction} from "./pages/Register"
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout/>,
-    loader: {homePageLoader}
-  },
-  {
-    path: '/home',
-    element: <Home/>
-  },
-  {
-    path: '/login',
-    element: <Login/>,
-    loader: loginPageAction
-  },
-  {
-    path: '/register',
-    element: <Register/>,
-    loader: registerPageAction
-  }
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
